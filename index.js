1b22499e54a6eb9f99716c5daf118cc6c1f16ff0
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-10-16 19:19:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ this.sex = sex;
 
 class Dog {
 constructor(name, sex)
+this.name = name;
+this.sex = sex;
+
 }
 
 class Bird {
   constructor(name, sex)
+  this.name = name;
+  this.sex = sex;
 
-}
+  }
